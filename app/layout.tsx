96613de90b12f54c4d377130a0927209e270dd2f
gaadaay81-design <gaadaay81@gmail.com>
2026-05-21 09:04:01 +0200

    Add metadata and change language to Somali

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'SahlanSuuq',
+  description: 'Wax ka iibso, wax ku iibso',
+}
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="so">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
